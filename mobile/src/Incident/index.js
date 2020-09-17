e83d0c906c16dc4b2c91dcc5567d804dc27fabf5
YannHyaric <yann@ice.ufjf.br>
2020-09-17 17:32:58 -0300

    commit teste

---
+++ mobile/src/Incident/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import {View} from 'react-native'
+import logo from '../assets/logo.png'
+
+export default function Incidents(){
+    return(
+        <View />
+    );
+}
