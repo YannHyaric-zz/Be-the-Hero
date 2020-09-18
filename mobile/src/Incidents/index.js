95c307b2f15c5ef1a80f06d5ba793a20491d6dfb
YannHyaric <yann@ice.ufjf.br>
2020-09-18 11:12:22 -0300

    api implement

--- mobile/src/Incidents/index.js
+++ mobile/src/Incidents/index.js
@@ -1,16 +1,27 @@
-import React from "react";
+import React,{useState, useEffect} from "react";
 import { View, FlatList, Image, Text, TouchableOpacity } from "react-native";
 import styles from "../Incidents/styles";
 import Logo from "../assets/Logo.png";
 import { Feather } from "@expo/vector-icons";
 import {useNavigation} from '@react-navigation/native'
+import api from '../services/api'
 
 export default function Incidents() {
+  const [incidents,setIncidents]=useState();
   const navigation = useNavigation();
 
   function navigateToDetail(){
     navigation.navigate('Detail');
   }
+
+  async function loadIncidents(){
+    const response= await api.get('incidents');
+    setIncidents(response.data)
+  }
+
+  useEffect(()=>{
+    loadIncidents
+  },[]);
 
   return (
     <View style={styles.container}>
@@ -26,13 +37,17 @@ export default function Incidents() {
 
       <FlatList
         style={styles.incidentList}
-        data={[1,2]}
-        keyExtractor={incident =>String(incident)}
+        data={incidents}
+        keyExtractor={incident =>String(incident.id)}
         showsVerticalScrollIndicator={false}
-        renderItem={() => (
+        renderItem={({item:incident}) => (
           <View style={styles.incident}>
             <Text style={styles.incidentProperty}>Ong:</Text>
-            <Text style={styles.incidentValue}>APAD </Text>
+            <Text style={styles.incidentValue}>{incident.name}</Text>
+            <Text style={styles.incidentProperty}>Caso:</Text>
+            <Text style={styles.incidentValue}>{incident.title}</Text>
+            <Text style={styles.incidentProperty}>Ong:</Text>
+            <Text style={styles.incidentValue}>{incident.value}</Text>
             <TouchableOpacity style={styles.detailsButton} onPress={navigateToDetail}>
               <Text style={styles.detailButtonText}>Ver mais detalhes</Text>
               <Feather name="arrow-right" size={16} color="#E02041" />
